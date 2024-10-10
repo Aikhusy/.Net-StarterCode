e52283209da39c71ef82f1fe727def6531c59409
Syaikhul Shafwan R <heymepoo@gmail.com>
2024-10-10 14:40:51 +0700

    fix : crud form, but the delete confirmations still ugly as fuck

--- obj/Debug/net8.0/PostsCrudApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/PostsCrudApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PostsCrudApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37e70fd1c6e1a4b9d75e44bc3496c519787f0aa9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+804eecff90c77c22d5001547fe8aa7a74a3a2cc3")]
 [assembly: System.Reflection.AssemblyProductAttribute("PostsCrudApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PostsCrudApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
